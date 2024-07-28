df84d171d9aaa173229c4ec809edb5c6092c5a61
flysap579 <flysap@gmail.com>
2024-07-28 21:43:31 +0800

    Update stock_price_notification_US.py

--- stock_price_notification_US.py
+++ stock_price_notification_US.py
@@ -19,7 +19,7 @@ def send_line_notify(message):
         print(f"Error sending Line Notify: {e}")
 
 # 股票代碼列表
-stocks = ['0050.TW', '00631L.TW', '00733.TW', 'AAPL','QLD','USD','BTC-USD']
+stocks = [ 'AAPL','QLD','USD','BTC-USD']
 
 def get_stock_info(symbol):
     stock = yf.Ticker(symbol)
